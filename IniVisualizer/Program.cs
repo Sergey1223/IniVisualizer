40c57fc8eb33c0d0066c4bbb97f08793c94d0e55
Shepard <Shepard@CAMARO>
2023-04-24 16:50:18 +0300

    Add project files.

---
+++ IniVisualizer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IniVisualizer
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            
+            IniServer.MainForm serverForm = new IniServer.MainForm();
+            serverForm.Show();
+
+            IniClient.MainForm clientForm = new IniClient.MainForm();
+            clientForm.Show();
+            
+            Application.Run();
+        }
+    }
+}
